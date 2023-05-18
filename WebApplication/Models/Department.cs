a28b5ea73317822f7e797777fc5367dd7f385cb4
Lucas Goncalves <lucasgnclvs1@gmail.com>
2023-05-17 21:40:54 -0300

    Seeding service

--- WebApplication/Models/Department.cs
+++ WebApplication/Models/Department.cs
@@ -27,7 +27,7 @@ namespace WebApplication.Models
         }
         public double totalSales(DateTime initial, DateTime final)
         {
-            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+            return Sellers.Sum(seller => seller.TotalSales(initial, final)); 
         }
     }
 }
